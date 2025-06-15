ec5f4d112fd53755ee4a2d47390961f3afc31252
Ashraful <ashrafulmomin7076@gmail.com>
2025-06-15 15:07:53 +0530

    Core Datatype in TypeScipt

---
+++ L1/app.ts
@@ -0,0 +1,10 @@
+//tsc have 3 type of core datatype number , boolean , string
+ let num1  = 25 ;
+ let num2 = 14; ;
+ let str = "hello";
+
+function sum(num1:number,num2 : number , str){
+    console.log(str)
+    return num1 + num2;
+}
+console.log(sum(num1,num2 , str));
