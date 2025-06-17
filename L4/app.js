e76391b8d2b994f5e2979272fff12dfe20736bef
Ashraful <ashrafulmomin7076@gmail.com>
2025-06-17 15:38:50 +0530

    function return type, callback

---
+++ L4/app.js
@@ -0,0 +1,10 @@
+console.log("working with functions in typescript");
+function sum(n1, n2) {
+    return n1 + n2;
+}
+function printName(name) {
+    console.log(name);
+}
+//this way we can specify return type
+// console.log(sum(1,2));
+console.log(printName("bitto"));
