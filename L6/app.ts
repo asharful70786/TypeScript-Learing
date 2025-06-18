e4fc81dd2fe7754d2e9be0c4a4c35e5de29a92c7
Ashraful <ashrafulmomin7076@gmail.com>
2025-06-18 14:28:05 +0530

    classes in ts

--- L6/app.ts
+++ L6/app.ts
@@ -1,8 +1,110 @@
-class Person {
-constructor(public name : string , public age : number){}
+//basic Class 
+// class Person {
+// constructor(public name : string , public age : number){
+//   this.name = name;
+//   this.age = age;
+// };
+//  describe(){
+//   console.log(`name is ${this.name} and age is ${this.age}`);
+//  }
+// }
+
+
+// const p1 = new Person("bitto" , 20 )
+
+// console.log(p1.describe());
+
+//private & public access modifier 
+
+// class department {
+//    name: string;
+//    age: number;
+//    public readonly id : number
+//   private salary: number;
+//   //when we assign privater its only accessible within class 'department'. 
+//    constructor(name: string, age: number, salary: number) {
+//       this.name = name;
+//       this.age = age;
+//       this.salary = salary
+//       this.id = 1
+//    }
+// }
+
+// let p1 = new department("bitto", 20, 30000);
+// console.log(p1) 
+
+
+//! Inheritence
+//! Override properties & protected access modifier
+//! setter & getter method
+class Department {
+    public name: string;
+    protected employees: string[];
+    private readonly id : string;
+    constructor(id: string, n :string) { //  shorthand
+        this.name = n;
+        this.id = id; // only once you can initialize
+        this.employees = [];
+    }
+    describe(this: Department) { 
+        console.log(`Department (${this.id}), ${this.name}`);
+    }
+
+    addEmployee(emp: string) {
+        this.employees.push(emp);
+    }
+    printEmployeeInformation() {
+        console.log('Number of emp: ', this.employees.length);
+        console.log(this.employees);
+    }
+
 }
 
+class AccountingDepartment extends Department {
+    // reports : string[] = [];
+    constructor(id: string, private reports : string[]){
+      super(id, "Accounting");
+    }
 
-const p1 = new Person("bitto" , 20);
+    addReports(text : string){
+        this.reports.push(text);
+    }
+    printReports(){
+        console.log(this.reports);
+    }
 
-console.log(p1);
+    addEmployee(emp: string): void {
+        if(emp === "Patel"){
+            return;
+        }
+        this.employees.push(emp);
+    }
+
+    get getReports(){
+        if(this.reports.length > 0){
+            return this.reports;
+        }
+        throw new Error("Report not found.")
+    }
+    set setReports(value : string){
+        if(!value){
+            throw new Error("Please pass valid value.")
+        }
+        this.reports.push(value);
+    }
+}
+
+const accDep = new AccountingDepartment("d1", []);
+accDep.addReports("Bugs");
+// accDep.printReports();
+// accDep.describe();
+
+accDep.addEmployee("Patel");
+accDep.addEmployee("Shivani");
+// accDep.printEmployeeInformation();
+
+// setter & getter
+console.log("getter -> ", accDep.getReports);
+accDep.setReports = "Code review error";
+console.log("getter -> ", accDep.getReports);
+ 
